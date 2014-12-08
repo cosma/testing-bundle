811aacc14b2de888c08c371c0c605a14fcbfaf0f
cvoicu-magari <cosmin.voicu@magari-internet.de>
2014-12-08 11:05:12 +0100

    good composer

--- Tests/TestCase/SimpleTestCaseTest.php
+++ Tests/TestCase/SimpleTestCaseTest.php
@@ -47,31 +47,12 @@ class SimpleTestCaseTest extends SimpleTestCase
 }
 
 
+
 class ExampleEntity {
 
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="id", type="integer")
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="AUTO")
-     */
     private $id;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="name", type="string", length=255)
-     */
     private $name;
-
-
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="email", type="string", length=255)
-     */
-    private $email;
 
     /**
      * @return mixed
@@ -79,22 +60,6 @@ class ExampleEntity {
     public function getId()
     {
         return $this->id;
-    }
-
-    /**
-     * @return string
-     */
-    public function getEmail()
-    {
-        return $this->email;
-    }
-
-    /**
-     * @param string $email
-     */
-    public function setEmail($email)
-    {
-        $this->email = $email;
     }
 
     /**
