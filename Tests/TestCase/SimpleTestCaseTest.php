39045ceca8d1679f51db653a2fcb1b7d118bd312
cvoicu-magari <cosmin.voicu@magari-internet.de>
2014-12-12 14:41:44 +0100

    comments added

--- Tests/TestCase/SimpleTestCaseTest.php
+++ Tests/TestCase/SimpleTestCaseTest.php
@@ -1,4 +1,17 @@
 <?php
+
+/**
+ * This file is part of the "cosma/testing-bundle" project
+ *
+ * (c) Cosmin Voicu<cosmin.voicu@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * Date: 11/07/14
+ * Time: 23:33
+ */
+
 namespace Cosma\Bundle\TestingBundle\Tests\TestCase;
 
 use Cosma\Bundle\TestingBundle\TestCase\SimpleTestCase;
