be3468fd2f619a22ee0ff64c6399f644a9be9012
cvoicu-magari <cosmin.voicu@magari-internet.de>
2014-12-18 17:19:48 +0100

    testLoadTableFixtures

--- Tests/TestCase/WebTestCaseTest.php
+++ Tests/TestCase/WebTestCaseTest.php
@@ -14,6 +14,7 @@
 
 namespace Cosma\Bundle\TestingBundle\Tests\TestCase;
 
+use Doctrine\DBAL\Exception\InvalidArgumentException;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bundle\FrameworkBundle\Client;
@@ -271,8 +272,6 @@ class WebTestCaseTest extends \PHPUnit_Framework_TestCase
         );
     }
 
-
-
     /**
      * @see Cosma\Bundle\TestingBundle\TestCase\WebTestCase::getMockedEntityWithId
      * @expectedException \Doctrine\ORM\EntityNotFoundException
@@ -283,13 +282,10 @@ class WebTestCaseTest extends \PHPUnit_Framework_TestCase
             ->disableOriginalConstructor()
             ->getMockForAbstractClass();
 
-
         $reflectionClass = new \ReflectionClass($webTestCase);
         $reflectionMethod = $reflectionClass->getMethod('getMockedEntityWithId');
         $reflectionMethod->setAccessible(true);
-
-        /** @var ExampleEntity $mockedEntity */
-        $mockedEntity = $reflectionMethod->invoke($webTestCase, 'Cosma\Bundle\TestingBundle\xxx', 12345);
+        $reflectionMethod->invoke($webTestCase, 'Cosma\Bundle\TestingBundle\xxx', 12345);
     }
 
     /**
@@ -301,7 +297,6 @@ class WebTestCaseTest extends \PHPUnit_Framework_TestCase
             ->disableOriginalConstructor()
             ->getMockForAbstractClass();
 
-
         $reflectionClass = new \ReflectionClass($webTestCase);
         $reflectionMethod = $reflectionClass->getMethod('getMockedEntityWithId');
         $reflectionMethod->setAccessible(true);
@@ -310,7 +305,6 @@ class WebTestCaseTest extends \PHPUnit_Framework_TestCase
         $mockedEntity = $reflectionMethod->invoke($webTestCase, 'Cosma\Bundle\TestingBundle\Tests\TestCase\SomeEntity', 12345);
 
         $this->assertInstanceOf('Cosma\Bundle\TestingBundle\Tests\TestCase\SomeEntity', $mockedEntity);
-
         $this->assertEquals(12345, $mockedEntity->getId());
     }
 
@@ -333,8 +327,6 @@ class WebTestCaseTest extends \PHPUnit_Framework_TestCase
         $reflectionMethod = $reflectionClass->getMethod('getMockedEntityWithId');
         $reflectionMethod->setAccessible(true);
 
-
-
         /** @var AnotherExampleEntity $mockedEntity */
         $mockedEntity = $reflectionMethod->invoke($webTestCase, 'AnotherExampleEntity', 12345);
 
@@ -342,6 +334,231 @@ class WebTestCaseTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf('Cosma\Bundle\TestingBundle\Tests\TestCase\AnotherExampleEntity', $mockedEntity);
     }
 
+    /**
+     * @see Cosma\Bundle\TestingBundle\TestCase\WebTestCase::getEntityWithId
+     * @expectedException \Doctrine\ORM\EntityNotFoundException
+     */
+    public function testGetEntityWithId_Exception()
+    {
+        $webTestCase = $this->getMockBuilder('Cosma\Bundle\TestingBundle\TestCase\WebTestCase')
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
+
+        $reflectionClass = new \ReflectionClass($webTestCase);
+        $reflectionMethod = $reflectionClass->getMethod('getEntityWithId');
+        $reflectionMethod->setAccessible(true);
+        $reflectionMethod->invoke($webTestCase, 'Cosma\Bundle\TestingBundle\xxx', 12345);
+    }
+
+    /**
+     * @see Cosma\Bundle\TestingBundle\TestCase\WebTestCase::getEntityWithId
+     */
+    public function testGetEntityWithId_FullNamespace()
+    {
+        $webTestCase = $this->getMockBuilder('Cosma\Bundle\TestingBundle\TestCase\WebTestCase')
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
+
+
+        $reflectionClass = new \ReflectionClass($webTestCase);
+        $reflectionMethod = $reflectionClass->getMethod('getEntityWithId');
+        $reflectionMethod->setAccessible(true);
+
+        /** @var ExampleEntity $entity */
+        $entity = $reflectionMethod->invoke($webTestCase, 'Cosma\Bundle\TestingBundle\Tests\TestCase\SomeEntity', 12345);
+
+        $this->assertInstanceOf('Cosma\Bundle\TestingBundle\Tests\TestCase\SomeEntity', $entity);
+
+        $this->assertEquals(12345, $entity->getId());
+    }
+
+    /**
+     * @see Cosma\Bundle\TestingBundle\TestCase\WebTestCase::getEntityWithId
+     */
+    public function testGetEntityWithId_NoNamespace()
+    {
+        $webTestCase = $this->getMockBuilder('Cosma\Bundle\TestingBundle\TestCase\WebTestCase')
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
+
+        $reflectionClassMocked = new \ReflectionClass($webTestCase);
+        $reflectionClass       = $reflectionClassMocked->getParentClass();
+
+        $entityNameSpaceProperty = $reflectionClass->getProperty('entityNameSpace');
+        $entityNameSpaceProperty->setAccessible(true);
+        $entityNameSpaceProperty->setValue('Cosma\Bundle\TestingBundle\Tests\TestCase');
+
+        $reflectionMethod = $reflectionClass->getMethod('getEntityWithId');
+        $reflectionMethod->setAccessible(true);
+
+        /** @var AnotherExampleEntity $entity */
+        $entity = $reflectionMethod->invoke($webTestCase, 'AnotherExampleEntity', 12345);
+
+        $this->assertEquals(12345, $entity->getId());
+        $this->assertInstanceOf('Cosma\Bundle\TestingBundle\Tests\TestCase\AnotherExampleEntity', $entity);
+    }
+
+
+
+    /**
+     * @see Cosma\Bundle\TestingBundle\TestCase\WebTestCase::loadTableFixtures
+     *
+     * @expectedException InvalidArgumentException
+     */
+    public function testLoadTableFixtures_Exception()
+    {
+        $webTestCase = $this->getMockBuilder('Cosma\Bundle\TestingBundle\TestCase\WebTestCase')
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
+
+        $reflectionClass = new \ReflectionClass($webTestCase);
+        $method          = $reflectionClass->getParentClass()->getMethod('loadTableFixtures');
+        $method->setAccessible(true);
+
+        $method->invoke($webTestCase, array());
+    }
+
+    /**
+     * @see Cosma\Bundle\TestingBundle\TestCase\WebTestCase::loadTableFixtures
+     */
+    public function testLoadTableFixtures()
+    {
+        $fixtureManager = $this->getMockBuilder('h4cc\AliceFixturesBundle\Fixtures\FixtureManagerInterface')
+            ->disableOriginalConstructor()
+            ->setMethods(array('persist', 'loadFiles'))
+            ->getMockForAbstractClass();
+        $fixtureManager->expects($this->any())
+            ->method('persist')
+            ->with()
+            ->will($this->returnValue(true));
+        $fixtureManager->expects($this->any())
+            ->method('loadFiles')
+            ->with(array(
+                'Cosma/Bundle/TestingBundle/FixtureDirectory/Table/SomeEntity.yml',
+                'Cosma/Bundle/TestingBundle/FixtureDirectory/Table/AnotherExampleEntity.yml'
+            ))
+            ->will($this->returnValue($this->getEntities()));
+
+        $valueMap = array(
+            array('testing_cosma.fixture_path', 'FixtureDirectory'),
+            array('testing_cosma.fixture_table_directory', 'Table')
+        );
+        $container = $this->getMockBuilder('Symfony\Component\DependencyInjection\ContainerInterface')
+            ->disableOriginalConstructor()
+            ->setMethods(array('get', 'getParameter'))
+            ->getMockForAbstractClass();
+        $container->expects($this->any())
+            ->method('getParameter')
+            ->will($this->returnValueMap($valueMap));
+        $container->expects($this->once())
+            ->method('get')
+            ->with('h4cc_alice_fixtures.manager')
+            ->will($this->returnValue($fixtureManager));
+
+        $currentBundle = $this->getMockBuilder('Symfony\Component\HttpKernel\Bundle\BundleInterface')
+            ->disableOriginalConstructor()
+            ->setMethods(array('getPath'))
+            ->getMockForAbstractClass();
+        $currentBundle->expects($this->once())
+            ->method('getPath')
+            ->will($this->returnValue('Cosma/Bundle/TestingBundle'));
+
+        $kernel = $this->getMockBuilder('Symfony\Component\HttpKernel\KernelInterface')
+            ->disableOriginalConstructor()
+            ->setMethods(array('getContainer', 'getBundles'))
+            ->getMockForAbstractClass();
+        $kernel->expects($this->atLeast(3))
+            ->method('getContainer')
+            ->will($this->returnValue($container));
+
+        $webTestCase = $this->getMockBuilder('Cosma\Bundle\TestingBundle\TestCase\WebTestCase')
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
+
+        $reflectionClass = new \ReflectionClass($webTestCase);
+
+        $kernelProperty = $reflectionClass->getProperty('kernel');
+        $kernelProperty->setAccessible(true);
+        $kernelProperty->setValue($kernel);
+
+        $currentBundleProperty = $reflectionClass->getParentClass()->getProperty('currentBundle');
+        $currentBundleProperty->setAccessible(true);
+        $currentBundleProperty->setValue($currentBundle);
+
+        $loadTableFixturesMethod          = $reflectionClass->getParentClass()->getMethod('loadTableFixtures');
+        $loadTableFixturesMethod->setAccessible(true);
+
+        $entities = $loadTableFixturesMethod->invoke($webTestCase, array('SomeEntity', 'AnotherExampleEntity'));
+
+        $this->assertEquals($this->getEntities(), $entities, 'Entities are wrong');
+    }
+
+
+
+    /**
+     * @see Cosma\Bundle\TestingBundle\TestCase\WebTestCase::loadTestFixtures
+     *
+     * @expectedException InvalidArgumentException
+     */
+    public function testLoadTestFixtures_Exception()
+    {
+        $webTestCase = $this->getMockBuilder('Cosma\Bundle\TestingBundle\TestCase\WebTestCase')
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
+
+        $reflectionClass = new \ReflectionClass($webTestCase);
+        $method          = $reflectionClass->getParentClass()->getMethod('loadTestFixtures');
+        $method->setAccessible(true);
+
+        $method->invoke($webTestCase, array());
+    }
+
+
+
+
+    /**
+     * @see Cosma\Bundle\TestingBundle\TestCase\WebTestCase::loadCustomFixtures
+     *
+     * @expectedException InvalidArgumentException
+     */
+    public function testLoadCustomFixtures_Exception()
+    {
+        $webTestCase = $this->getMockBuilder('Cosma\Bundle\TestingBundle\TestCase\WebTestCase')
+            ->disableOriginalConstructor()
+            ->getMockForAbstractClass();
+
+        $reflectionClass = new \ReflectionClass($webTestCase);
+        $method          = $reflectionClass->getParentClass()->getMethod('loadCustomFixtures');
+        $method->setAccessible(true);
+
+        $method->invoke($webTestCase, array());
+    }
+
+
+    /**
+     * @return array
+     */
+    private function getEntities()
+    {
+        $objects   = array();
+        $entityOne = new SomeEntity();
+        $entityOne->setName('Some Entity One');
+        array_push($objects, $entityOne);
+
+        $entityTwo = new SomeEntity();
+        $entityTwo->setName('Some Entity Two');
+        array_push($objects, $entityTwo);
+
+        $entityThree = new AnotherExampleEntity();
+        $entityThree->setFirstName('Example Entity Three');
+        array_push($objects, $entityThree);
+
+        $entityFour = new AnotherExampleEntity();
+        $entityFour->setFirstName('Example Entity Four');
+        array_push($objects, $entityFour);
+
+        return $objects;
+    }
 
 
 }
