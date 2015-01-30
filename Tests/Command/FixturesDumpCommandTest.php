bb8d081b4de852ff6030af29275811ecb41928d3
Cosmin Voicu <cosmin.voicu@crobo.com>
2015-01-30 11:29:10 +0100

    fix tests and readme

--- Tests/Command/FixturesDumpCommandTest.php
+++ Tests/Command/FixturesDumpCommandTest.php
@@ -25,6 +25,7 @@ use Symfony\Component\Console\Output\BufferedOutput;
 use Symfony\Component\Console\Output\ConsoleOutput;
 use Symfony\Component\Console\Output\NullOutput;
 use Symfony\Component\Console\Output\StreamOutput;
+use Symfony\Component\DependencyInjection\Container;
 
 class FixturesDumpCommandTest extends \PHPUnit_Framework_TestCase
 {
@@ -66,10 +67,7 @@ class FixturesDumpCommandTest extends \PHPUnit_Framework_TestCase
      */
     public function testExecute_SpecificEntity_Association()
     {
-        $this->markTestSkipped('For the moment');
-
         $directoryPath = sys_get_temp_dir();
-
 
         $dumper = $this->getMockBuilder('\Cosma\Bundle\TestingBundle\Fixture\Dumper')
             ->disableOriginalConstructor()
@@ -118,25 +116,17 @@ class FixturesDumpCommandTest extends \PHPUnit_Framework_TestCase
             ->method('getManager')
             ->will($this->returnValue($entityManager));
 
-
-        $container = $this->getMockBuilder('\Symfony\Component\DependencyInjection\ContainerInterface')
-            ->disableOriginalConstructor()
-            ->setMethods(array('get'))
-            ->getMockForAbstractClass();
-        $container->expects($this->any())
-            ->method('get')
-            ->will($this->returnValue($doctrine));
+        $container = new Container();
+        $container->set('doctrine', $doctrine);
+        $container->set('cosma_testing.fixture_dumper', $dumper);
 
         $command = $this->getMockBuilder('\Cosma\Bundle\TestingBundle\Command\FixturesDumpCommand')
             ->disableOriginalConstructor()
-            ->setMethods(array('getContainer', 'getDumper'))
+            ->setMethods(array('getContainer'))
             ->getMock();
-        $command->expects($this->exactly(4))
+        $command->expects($this->exactly(2))
             ->method('getContainer')
             ->will($this->returnValue($container));
-        $command->expects($this->once())
-            ->method('getDumper')
-            ->will($this->returnValue($dumper));
 
         $reflectionClass = new \ReflectionClass($command);
 
@@ -176,8 +166,6 @@ class FixturesDumpCommandTest extends \PHPUnit_Framework_TestCase
      */
     public function testExecute_NoSpecificEntity_NoAssociation()
     {
-        $this->markTestSkipped('For the moment');
-
         $directoryPath = sys_get_temp_dir();
 
         $dumper = $this->getMockBuilder('\Cosma\Bundle\TestingBundle\Fixture\Dumper')
@@ -190,6 +178,7 @@ class FixturesDumpCommandTest extends \PHPUnit_Framework_TestCase
             ->will($this->returnValue($directoryPath . '/table.yml'));
         $dumper->expects($this->once())
             ->method('setAssociation')
+            ->with(FALSE)
             ->will($this->returnValue(NULL));
 
         $classMetaDataInfo = $this->getMockBuilder('\Doctrine\ORM\Mapping\ClassMetadataInfo')
@@ -228,31 +217,18 @@ class FixturesDumpCommandTest extends \PHPUnit_Framework_TestCase
             ->method('getManager')
             ->will($this->returnValue($entityManager));
 
-        $container = $this->getMockBuilder('\Symfony\Component\DependencyInjection\ContainerInterface')
-            ->disableOriginalConstructor()
-            ->setMethods(array('get'))
-            ->getMockForAbstractClass();
-        $container->expects($this->once())
-            ->method('get')
-            ->with('doctrine')
-            ->will($this->returnValue($doctrine));
+        $container = new Container();
+        $container->set('doctrine', $doctrine);
+        $container->set('cosma_testing.fixture_dumper', $dumper);
 
         $command = $this->getMockBuilder('\Cosma\Bundle\TestingBundle\Command\FixturesDumpCommand')
             ->disableOriginalConstructor()
             ->setMethods(array('getContainer'))
             ->getMock();
-        $command->expects($this->once())
+        $command->expects($this->exactly(2))
             ->method('getContainer')
             ->will($this->returnValue($container));
-        $command->expects($this->once())
-            ->method('getDumper')
-            ->will($this->returnValue($dumper));
 
-        $reflectionClass = new \ReflectionClass($command);
-
-        $dumperProperty = $reflectionClass->getParentClass()->getProperty('dumper');
-        $dumperProperty->setAccessible(TRUE);
-        $dumperProperty->setValue($command, $dumper);
 
         $inputDefinition = new InputDefinition(array(
             new InputArgument('dumpDirectory', InputArgument::REQUIRED),
