dcb736afc5775f20f267d3f8964c2f3725f2dc9c
Cosmin Voicu <cosmin.voicu@ocono.de>
2015-10-23 00:58:15 +0200

    work in progress

--- TestCase/Traits/WebTestTrait.php
+++ TestCase/Traits/WebTestTrait.php
@@ -23,44 +23,6 @@ use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 trait WebTestTrait
 {
     /**
-     * @return void
-     */
-    public static function setUpBeforeClass()
-    {
-        static::bootKernel();
-
-        static::getCurrentBundle();
-        static::getFixtureManager();
-        static::getFixturePath();
-    }
-
-    /**
-     * Clean up Kernel usage in this test.
-     */
-    public static function tearDownAfterClass()
-    {
-
-        static::ensureKernelShutdown();
-    }
-
-    /**
-     * @return void
-     */
-    protected function setUp()
-    {
-        static::bootKernel();
-    }
-
-    /**
-     * @return void
-     */
-    protected function tearDown()
-    {
-        parent::tearDown();
-        \Mockery::close();
-    }
-
-    /**
      * @param array $server
      *
      * @return Client
@@ -75,15 +37,13 @@ trait WebTestTrait
         return $client;
     }
 
-
-
     /**
-     * @param array $debugTrace
-     *
-     * @return mixed
+     * @return mixed|string
      */
-    protected function getTestClassPath(array $debugTrace)
+    protected function getTestClassPath()
     {
+        $debugTrace = debug_backtrace();
+
         if (isset($debugTrace[0]['file'])) {
             $testPath = strpos($debugTrace[0]['file'], "Tests/", 1);
             $filePath = substr($debugTrace[0]['file'], $testPath + 6);
@@ -95,179 +55,5 @@ trait WebTestTrait
         return $testClassPath;
     }
 
-    /**
-     * @return BundleInterface
-     */
-    private static function getCurrentBundle()
-    {
-        if (NULL === self::$currentBundle) {
-            $bundles = static::$kernel->getBundles();
-            $currentTestClass = get_called_class();
 
-            foreach ($bundles as $bundle) {
-                if (0 === strpos($currentTestClass, $bundle->getNamespace())) {
-                    self::$currentBundle = $bundle;
-                }
-            }
-        }
-
-        return self::$currentBundle;
-    }
-
-    /**
-     * @param $bundleName
-     *
-     * @return \Symfony\Component\HttpKernel\Bundle\BundleInterface
-     * @throws \Exception
-     */
-    private function getBundleByName($bundleName)
-    {
-        $bundles = static::$kernel->getBundles();
-        foreach ($bundles as $bundle) {
-            if ($bundleName == $bundle->getName()) {
-                return $bundle;
-            }
-        }
-        throw new \Exception("Bundle not found: {$bundle}");
-    }
-
-    /**
-     * @return FixtureManager
-     */
-    private static function getFixtureManager()
-    {
-        if (NULL === self::$fixtureManager) {
-            self::$fixtureManager = static::$kernel->getContainer()->get('h4cc_alice_fixtures.manager');
-        }
-
-        return self::$fixtureManager;
-    }
-
-    /**
-     * @return string
-     */
-    private static function getFixturePath()
-    {
-        if (NULL === self::$fixturePath) {
-            $fixturePath = static::getCurrentBundle()->getPath() . '/' .
-                           static::$kernel->getContainer()->getParameter('cosma_testing.fixture_path');
-
-            self::$fixturePath = $fixturePath;
-        }
-
-        return self::$fixturePath;
-    }
-
-    /**
-     * @param $entity
-     *
-     * @return mixed
-     */
-    private function getFullPathEntity($entity)
-    {
-
-        if (FALSE !== strpos($entity, '\\')) {
-            return $entity;
-        }
-
-        /** @var EntityManager $entityManager */
-        $entityManager = static::$kernel->getContainer()->get('doctrine')->getManager();
-
-        /** @var ClassMetadataFactory $metadataFactory */
-        $metadataFactory = $entityManager->getMetadataFactory();
-
-        if (FALSE !== strpos($entity, ':')) {
-            $entityDescription = explode(':', $entity);
-
-            $bundleName = $entityDescription[0];
-            $entityName = $entityDescription[1];
-
-            $bundle = $this->getBundleByName($bundleName);
-
-            $fullPathEntity = $this->getEntityNamespaceForBundle($bundle, $metadataFactory) .
-                              '\\' .
-                              $entityName;
-
-            return $fullPathEntity;
-        }
-
-        $fullPathEntity = $this->getEntityNamespaceForBundle(static::getCurrentBundle(), $metadataFactory) .
-                          '\\' .
-                          $entity;
-
-        return $fullPathEntity;
-    }
-
-    /**
-     * @param \Symfony\Component\HttpKernel\Bundle\BundleInterface      $bundle
-     * @param \Doctrine\Common\Persistence\Mapping\ClassMetadataFactory $metadataFactory
-     *
-     * @return string
-     */
-    private function getEntityNamespaceForBundle(BundleInterface $bundle,ClassMetadataFactory $metadataFactory)
-    {
-
-        $metadataCollection = $metadataFactory->getAllMetadata();
-
-        /** @var ClassMetadata $metadata */
-        foreach ($metadataCollection as $metadata) {
-            if (0 === strpos($metadata->namespace, $bundle->getNamespace())) {
-                return $metadata->namespace;
-            }
-        }
-    }
-
-    /**
-     * @param array $fixtures
-     *
-     * @return array
-     */
-    private function appendTableFixturesPath(array $fixtures)
-    {
-        $fixturePath = static::getFixturePath() . '/';
-        $fixturePath .= static::$kernel->getContainer()->getParameter('cosma_testing.fixture_table_directory');
-
-        $fixturePaths = array();
-        foreach ($fixtures as $fixture) {
-            $fixturePaths[] = "{$fixturePath}/{$fixture}.yml";
-        }
-
-        return $fixturePaths;
-    }
-
-    /**
-     * @param array $fixtures
-     * @param       $testClassPath
-     *
-     * @return array
-     */
-    private function appendTestFixturesPath(array $fixtures, $testClassPath)
-    {
-        $fixturePath = static::getFixturePath() . '/';
-        $fixturePath .= static::$kernel->getContainer()->getParameter('cosma_testing.fixture_test_directory') . '/';
-        $fixturePath .= $testClassPath;
-
-        $fixturePaths = array();
-        foreach ($fixtures as $fixture) {
-            $fixturePaths[] = "{$fixturePath}/{$fixture}.yml";
-        }
-
-        return $fixturePaths;
-    }
-
-    /**
-     * @param array $fixtures
-     *
-     * @return array
-     */
-    private function appendCustomFixturesPath(array $fixtures)
-    {
-
-        $fixturePaths = array();
-        foreach ($fixtures as $tableFixture) {
-            $fixturePaths[] = static::getCurrentBundle()->getPath() . '/' . $tableFixture.'.yml';
-        }
-
-        return $fixturePaths;
-    }
 }
--- TestCase/WebTestCase.php
+++ TestCase/WebTestCase.php
@@ -15,13 +15,8 @@ namespace Cosma\Bundle\TestingBundle\TestCase;
 
 use Cosma\Bundle\TestingBundle\TestCase\Traits\SimpleTestTrait;
 use Cosma\Bundle\TestingBundle\TestCase\Traits\WebTestTrait;
-use Doctrine\Common\Persistence\Mapping\ClassMetadataFactory;
-use Doctrine\ORM\Mapping\ClassMetadata;
-use Doctrine\ORM\EntityNotFoundException;
 use Symfony\Bundle\FrameworkBundle\Client;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase as WebTestCaseBase;
-use Doctrine\ORM\EntityManager;
-use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 
 abstract class WebTestCase extends WebTestCaseBase
 {
@@ -75,26 +70,4 @@ abstract class WebTestCase extends WebTestCaseBase
 
         return $client;
     }
-
-
-
-    /**
-     * @param array $debugTrace
-     *
-     * @return mixed
-     */
-    protected function getTestClassPath(array $debugTrace)
-    {
-        if (isset($debugTrace[0]['file'])) {
-            $testPath = strpos($debugTrace[0]['file'], "Tests/", 1);
-            $filePath = substr($debugTrace[0]['file'], $testPath + 6);
-            $testClassPath = str_replace('.php', '', $filePath);
-        } else {
-            $testClassPath = '';
-        }
-
-        return $testClassPath;
-    }
-
-
 }
