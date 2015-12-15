d9e3ce00e116d82fd61fd49aa157a7d0028480be
Cosmin Voicu <cosmin.voicu@ocono.de>
2015-12-15 03:12:11 +0100

    get fixture real path

--- TestCase/Traits/DBTestTrait.php
+++ TestCase/Traits/DBTestTrait.php
@@ -7,7 +7,7 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  *
- * Date: 18/10/15
+ * Date: 11/12/15
  * Time: 18:30
  */
 
@@ -15,6 +15,7 @@ namespace Cosma\Bundle\TestingBundle\TestCase\Traits;
 
 use Doctrine\ORM\EntityManager;
 use h4cc\AliceFixturesBundle\Fixtures\FixtureManager;
+use Symfony\Component\HttpKernel\Bundle\BundleInterface;
 
 trait DBTestTrait
 {
@@ -62,6 +63,51 @@ trait DBTestTrait
     }
 
     /**
+     * @param array $fixtures
+     * @param       $dropDatabaseBefore
+     *
+     * @return array
+     */
+    protected function loadFixtures(array $fixtures, $dropDatabaseBefore = true)
+    {
+        $fixtureManager = static::getFixtureManager();
+        if ($dropDatabaseBefore) {
+            $fixtureManager->persist([], true);
+        }
+
+        $fixturesFiles = array_map([$this, 'getFixtureFile'], $fixtures);
+
+        $objects = $fixtureManager->loadFiles($fixturesFiles);
+        $fixtureManager->persist($objects);
+
+        return $objects;
+    }
+
+    /**
+     * @param $fixture
+     *
+     * @return string
+     * @throws \Exception
+     */
+    private function getFixtureFile($fixture)
+    {
+        $pathParts = explode(':', $fixture);
+        if (!is_array($pathParts) && count($pathParts) < 2) {
+            throw new \Exception("You are trying to load a wrong fixture - {$fixture}");
+        }
+
+        $bundleName = array_shift($pathParts);
+
+        $bundlePath = $this->getBundlePath($bundleName);
+
+        $fixtureDirectory = static::$kernel->getContainer()->getParameter('cosma_testing.fixture_directory');
+
+        $innerBundlePath = implode(DIRECTORY_SEPARATOR, $pathParts);
+
+        return $bundlePath . DIRECTORY_SEPARATOR . $fixtureDirectory . DIRECTORY_SEPARATOR . $innerBundlePath . '.yml';
+    }
+
+    /**
      * @return FixtureManager
      */
     private static function getFixtureManager()
@@ -74,20 +120,22 @@ trait DBTestTrait
     }
 
     /**
-     * @param array $fixtures
-     * @param       $dropDatabaseBefore
+     * @param $bundleName
      *
-     * @return array
+     * @return string
+     * @throws \Exception
      */
-    protected function loadFixtures(array $fixtures, $dropDatabaseBefore = true)
+    private function getBundlePath($bundleName)
     {
-        $fixtureManager = static::getFixtureManager();
-        if ($dropDatabaseBefore) {
-            $fixtureManager->persist([], true);
-        }
-        $objects = $fixtureManager->loadFiles($fixtures);
-        $fixtureManager->persist($objects);
+        $bundles = static::$kernel->getBundles();
 
-        return $objects;
+        /** @type BundleInterface $bundle */
+        foreach ($bundles as $bundle) {
+            if ($bundleName == $bundle->getName()) {
+                return $bundle->getPath();
+            }
+        }
+
+        throw new \Exception("This bundle {$bundleName} doesn't exists");
     }
 }
